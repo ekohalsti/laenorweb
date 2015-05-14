394412ca55b2f753eb22491bfa22dc07ced66ea0
Esa Halsti <esa.halsti@gmail.com>
2015-05-14 17:42:44 +0300

    database setup, area model activation

--- area/models.py
+++ area/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class description(models.Model):    
+    description_text = models.TextField()
+    pub_date = models.DateTimeField('date published')
+    submitter = models.CharField(max_length=300)
+
+
+class area_map(models.Model):
+    map_field = models.TextField()
+    notes = models.TextField()
+
+
+class quest(models.Model):
+    description_text = models.TextField()
